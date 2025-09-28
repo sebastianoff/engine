7367f3a8cf07838ab0c30b6ce5ee39ba24aeb1b0
sebastianoff <cheesecakeycat@protonmail.com>
2025-09-28 22:27:14 +0300

    Better error reporting.

--- src/engine/Window.zig
+++ src/engine/Window.zig
@@ -17,28 +17,52 @@ pub const Config = struct {
 
 pub fn init(config: Config) !Window {
     if (!sdl.SDL_InitSubSystem(sdl.SDL_INIT_VIDEO)) {
-        return error.FailedToInitSdl;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.init at SDL_InitSubSystem)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_InitSubSystem;
     }
     var flags: sdl.Uint32 = sdl.SDL_WINDOW_HIDDEN;
     if (config.resizable) flags |= sdl.SDL_WINDOW_RESIZABLE;
     if (config.highdpi) flags |= sdl.SDL_WINDOW_HIGH_PIXEL_DENSITY;
     const window = sdl.SDL_CreateWindow(config.title, @intCast(config.width), @intCast(config.height), flags) orelse {
         sdl.SDL_QuitSubSystem(sdl.SDL_INIT_VIDEO);
-        return error.FailedToInitWindw;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.init at SDL_CreateWindow)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_CreateWindow;
     };
     // init the device
     const name_ptr: [*c]const u8 = if (config.driver_name) |name| name else null;
     const device = sdl.SDL_CreateGPUDevice(config.shader_formats, config.debug_gpu, name_ptr) orelse {
         sdl.SDL_DestroyWindow(window);
         sdl.SDL_QuitSubSystem(sdl.SDL_INIT_VIDEO);
-        return error.FailedToInitGpu;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.init at SDL_CreateGPUDevice)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_CreateGPUDevice;
     };
     // claim the window
     if (!sdl.SDL_ClaimWindowForGPUDevice(device, window)) {
         sdl.SDL_DestroyGPUDevice(device);
         sdl.SDL_DestroyWindow(window);
         sdl.SDL_QuitSubSystem(sdl.SDL_INIT_VIDEO);
-        return error.FailedToClaimWindow;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.init at SDL_ClaimWindowForGPUDevice)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_ClaimWindowForGPUDevice;
     }
 
     _ = sdl.SDL_SetGPUAllowedFramesInFlight(device, config.frames_in_flight);
@@ -58,7 +82,13 @@ pub fn clearColor(window: *Window, r: f32, g: f32, b: f32, a: f32) !void {
     var swapchain_width: sdl.Uint32 = 0;
     if (!sdl.SDL_WaitAndAcquireGPUSwapchainTexture(command, window.ptr, &swapchain_texture, &swapchain_width, &swapchain_height)) {
         _ = sdl.SDL_CancelGPUCommandBuffer(command);
-        return error.FailedToAcquireSwapchain;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.clearColor at SDL_WaitAndAcquireGPUSwapchainTexture)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_WaitAndAcquireGPUSwapchainTexture;
     }
     var color_target: sdl.SDL_GPUColorTargetInfo = .{
         .texture = swapchain_texture.?,
@@ -73,12 +103,27 @@ pub fn clearColor(window: *Window, r: f32, g: f32, b: f32, a: f32) !void {
         .cycle = false,
         .cycle_resolve_texture = false,
     };
+    todo.call();
     const render_pass = sdl.SDL_BeginGPURenderPass(command, &color_target, 1, null) orelse {
         _ = sdl.SDL_CancelGPUCommandBuffer(command);
-        return error.FailedToBeginRenderPass;
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.clearColor at SDL_BeginGPURenderPass)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_BeginGPURenderPass;
     };
     sdl.SDL_EndGPURenderPass(render_pass);
-    if (!sdl.SDL_SubmitGPUCommandBuffer(command)) return error.FailedToSubmit;
+    if (!sdl.SDL_SubmitGPUCommandBuffer(command)) {
+        writer.err("{[err]s} ({[file]s}{[line]d}:{[column]d}: Window.clearColor at SDL_SubmitGPUCommandBuffer)", .{
+            .file = @src().file,
+            .line = @src().line,
+            .column = @src().column,
+            .err = sdl.SDL_GetError(),
+        });
+        return error.SDL_SubmitGPUCommandBuffer;
+    }
 }
 
 pub fn deinit(window: *Window) void {
@@ -89,6 +134,14 @@ pub fn deinit(window: *Window) void {
     sdl.SDL_QuitSubSystem(sdl.SDL_INIT_VIDEO);
 }
 
+var todo = std.once(struct {
+    fn todoDynamicRendering() void {
+        writer.info("TODO: use dynamic rendering when it will be available in SDL", .{});
+    }
+}.todoDynamicRendering);
+
+const std = @import("std");
 const sdl = @import("sdl");
 const builtin = @import("builtin");
+const writer = @import("writer.zig");
 const Window = @This();
