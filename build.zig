77d39bc9f675b3007390336aec62b1cd97f87bb8
sebastianoff <cheesecakeycat@protonmail.com>
2025-10-13 20:56:34 +0300

    Add SDL.

--- build.zig
+++ build.zig
@@ -16,6 +16,18 @@ pub fn build(b: *std.Build) void {
 
     const root_raw = b.option([]const u8, "root", "Root of the module that will be passed to the runner to execute") orelse std.debug.panic("the required 'root' option was not passed", .{});
     options.addOption([]const u8, "root", root_raw);
+    // sdl
+    const sdl_dep = b.dependency("sdl", .{
+        .target = target,
+        .optimize = optimize,
+        .preferred_linkage = .dynamic,
+    });
+    const sdl_translate_c = b.addTranslateC(.{
+        .root_source_file = sdl_dep.path("include/SDL3/SDL.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    sdl_translate_c.addIncludePath(sdl_dep.path("include"));
     // there's a weird segmentation fault on linux that happens on any functions
     // that call `std.debug.lockStderrWriter` if you do not link libc on both
     // runner and the module.
@@ -34,6 +46,16 @@ pub fn build(b: *std.Build) void {
         }),
     });
     b.installArtifact(start_exe);
+    // engine module
+    const engine_mod = b.addModule("engine", .{
+        .root_source_file = b.path("src/engine/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{
+            .{ .name = "sdl", .module = sdl_translate_c.createModule() },
+        },
+    });
+    engine_mod.linkLibrary(sdl_dep.artifact("SDL3"));
     // the module itself
     const root_lib = b.addLibrary(.{
         .name = basename_raw,
@@ -43,6 +65,9 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
             .link_libc = link_libc,
+            .imports = &.{
+                .{ .name = "engine", .module = engine_mod },
+            },
         }),
     });
     b.installArtifact(root_lib);
